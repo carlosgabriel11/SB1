47b514bc66edbd36a54d7f490c5178f360aad8e9
carlosgabriel11 <cgaa1103@gmail.com>
2018-10-05 10:43:39 -0300

    created the class of the preprocessing

--- montador.cpp
+++ montador.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <stdio.h>
+#include <string>
 
 using namespace std;
+
+//this class will have the function to preprocess the target file
+class Preprocessor{
+    //the pointer of the target file
+    FILE* target;
+    //the pointer to the new file created to contain
+    //the target after the preprocessing
+    FILE* pre;
+
+public:
+    //the constructor to open the file
+    Preprocessor(const string&);
+    //the destructor to close the file
+    ~Preprocessor();
+    //remove comments of the file
+    void removeComments();
+
+};
 
 //start of the main function
 int main(int argc, char** argv){
